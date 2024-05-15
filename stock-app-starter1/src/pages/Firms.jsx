bf19d7cb52ba8d70d1e90f9e2806dc77b0eb594c
taha_ murat <ttahamuratdoser@gmail.com>
2024-05-15 15:25:02 +0300

    added firmModal

--- stock-app-starter1/src/pages/Firms.jsx
+++ stock-app-starter1/src/pages/Firms.jsx
@@ -1,13 +1,17 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useStockRequest from "../services/useStockRequest";
 import { useSelector } from "react-redux";
 import Typography from "@mui/material/Typography";
 import { Button, Grid } from "@mui/material";
 import FirmCard from "../components/FirmCard";
+import FirmModal from "../components/FirmModal";
 
 const Firms = () => {
   const { getStock } = useStockRequest();
   const { firms } = useSelector((state) => state.stock);
+  const [open, setOpen] = useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
   useEffect(() => {
     /* getStock("sales") */
     getStock("firms");
@@ -18,7 +22,8 @@ const Firms = () => {
       <Typography variant="h4" color={"error"} mt={4} mb={4}>
         Firms
       </Typography>
-      <Button variant="contained">New Firm</Button>
+      <Button variant="contained" onClick={handleOpen} >New Firm</Button>
+      <FirmModal handleClose ={handleClose} open={open} />
 
       <Grid container gap={2} mt={3} justifyContent={"center"}>
         {firms.map((firm) => (
